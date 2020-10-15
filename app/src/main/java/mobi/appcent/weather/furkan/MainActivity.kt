96446939f10d97cf34be37a5676ba95140737028
Furkan Uzum <furkanuzum61@gmail.com>
2020-10-15 15:51:35 +0300

    initial commit

---
+++ app/src/main/java/mobi/appcent/weather/furkan/MainActivity.kt
@@ -0,0 +1,18 @@
+package mobi.appcent.weather.furkan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mobi.appcent.weather.furkan.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/mobi/appcent/weather/furkan/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package mobi.appcent.weather.furkan.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
